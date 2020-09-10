f20483cf5f9db7dc374c68e5dac41c08c42ff44b
iana-davydenko <iana.lady1302@gmail.com>
2020-09-10 10:52:28 +0300

    some changes

---
+++ list2.py
@@ -0,0 +1,8 @@
+thislist = ['dress', 'skirt', 'shirt', 'shorts']
+thislist.append('shoes')
+for x in thislist:
+     print(x)
+
+thislist = ['dress', 'skirt', 'shirt', 'shorts']
+thislist.pop()
+print(thislist)
---
+++ lists.py
@@ -0,0 +1,9 @@
+thislist = ['dress', 'skirt', 'shirt', 'shorts']
+thislist.append('shoes')
+
+print(thislist)
+
+thislist = ['dress', 'skirt', 'shirt', 'shorts']
+del thislist[3]
+
+print(thislist)
